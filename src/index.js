2c48d507cd8b1d71f087025018cfc2c1da183344
Ayush <ayushdhokriya823@gmail.com>
2023-05-03 12:06:37 +0530

    audio added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,73 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
-  state = {
-    keys: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"],
+const sounds = [
+  {
+    key: "Q",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    key: "W",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    key: "E",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    key: "A",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    key: "S",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    key: "D",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    key: "Z",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    key: "X",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+  {
+    key: "C",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+];
+
+const App = (props) => (
+  <div id="drum-machine" className="container">
+    <div id="display" className="display">
+      {sounds.map((sound, idx) => (
+        <Box text={sound.key} key={idx} audio={sound.mp3} />
+      ))}
+    </div>
+  </div>
+);
+
+class Box extends React.Component {
+  constructor(props) {
+    super(props);
+    this.audio = React.createRef();
+  }
+
+  playSound = () => {
+    this.audio.current.play();
   };
   render() {
-    const { keys } = this.state;
+    const { text, audio } = this.props;
     return (
-      <div id="drum-machine" className="container">
-        <div id="display" class="display">
-          {keys.map((key, idx) => (
-            <Box text={key} key={idx} />
-          ))}
-        </div>
+      <div className="box" onClick={this.playSound}>
+        {text}
+        <audio ref={this.audio} src={audio} className="clip" id={text} />
       </div>
     );
   }
 }
 
-const Box = (props) => <div className="box">{props.text}</div>;
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App keys={keys} />, document.getElementById("root"));
